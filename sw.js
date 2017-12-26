08fa4e330f66c2dd8452de2f044792b1cfa2039c
aquilax <aquilax@gmail.com>
2017-12-26 09:27:46 +0100

    PWA

---
+++ sw.js
@@ -0,0 +1,38 @@
+var staticCacheName = 'background-generator-v1';
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/index.html?homescreen=1',
+                '/?homescreen=1',
+                '/sw.js'
+            ])
+                .then(() => self.skipWaiting());
+        })
+    )
+});
+
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function (cacheName) {
+                    return cacheName.startsWith('scrum-poker') && cacheName != staticCacheName;
+                }).map(function (cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
